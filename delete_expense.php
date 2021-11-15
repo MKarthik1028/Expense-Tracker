03c3adcf0707a0c72af90f23000b0b5e3275e89c
azileify <azileify7@gmail.com>
2021-11-15 16:53:34 +0530

    Added Category and Expenses Deletion Support

---
+++ delete_expense.php
@@ -0,0 +1,10 @@
+<?php
+include './functions.php';
+
+$expense_id = $params['expense_id'];
+
+$db->delete("expenses", array("id"=>$expense_id));
+
+$_SESSION['SUCCESS'] = "Deleted successfully";
+header("Location: manage_expenses.php");
+die;
