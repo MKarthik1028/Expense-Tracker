951d350ec7cec1b1e377b2612c836f8990dd14f6
azileify <azileify7@gmail.com>
2021-11-15 11:49:30 +0530

    Added Homepage

---
+++ footer.php
@@ -0,0 +1,15 @@
+<section class="footer1 cid-sENJRfrqYE" once="footers" id="footer01-5" style="background-color: #fff">
+	    <div class="container">
+		<div class="row align-left mbr-white">
+		    <div class="col-lg-3">
+			<p class="mbr-title mb-0 mbr-fonts-style display-5"><strong>Expense Tracker</strong><br>
+			</p>
+		    </div>
+		    <div class="col-lg-9">
+			<p class="mbr-text mb-0 mbr-fonts-style display-4">
+			    © Copyright 2021 Expense Tracker - All Rights Reserved.<br>
+			</p>
+		    </div>
+		</div>
+	    </div>
+	</section>
